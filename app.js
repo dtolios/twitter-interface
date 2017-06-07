386d848b6c7cc85fdaeaf7516570c834fddea1c6
Dimitri Tolios <dtolios@riotgames.com>
2017-06-06 17:18:29 -0700

    Added functionality for getting tweet and friend data using twit in middleware

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const Twit = require('twit');
+
 const config = require('./config.js');
 
 app.set('views', './templates');
@@ -8,8 +10,43 @@ app.set('view engine', 'pug');
 
 app.use('/public', express.static('public'));
 
+
+const requestTweets = function(req, res, next) {
+	const T = new Twit(config);
+
+	T.get('statuses/user_timeline', { screen_name: 'dtolios', count: 5 }, function(err, data) {
+        req.requestTweets = data;
+        next();
+	});
+
+};
+
+const requestFriends = function(req, res, next) {
+    const T = new Twit(config);
+
+    T.get('friends/list', { screen_name: 'dtolios', count: 5 }, function(err, data) {
+        req.requestFriends = data.users;
+        next();
+    });
+
+};
+
+app.use(requestTweets);
+app.use(requestFriends);
+
 app.get('/', function(req, res) {
-	res.render('index', { title: 'Twitter Interface'});
+	res.render('index', { tweets : req.requestTweets , friends : req.requestFriends });
+});
+
+app.use(function(req, res, next) {
+	res.status(404);
+	res.render('404');
+});
+
+app.use(function(err, req, res, next){
+	console.error(err.stack);
+	res.status(500);
+	res.render('500');
 });
 
 app.listen(3000, function() {
